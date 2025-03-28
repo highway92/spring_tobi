6a736f408e829d9d32b9ca4f79152540ed1ddba2
highway92 <highway92@kakao.com>
2025-03-28 13:11:49 +0900

    addStrategy 적용

--- src/main/java/com/springbook/tobi/user/UserDao.java
+++ src/main/java/com/springbook/tobi/user/UserDao.java
@@ -17,14 +17,7 @@ public class UserDao {
 
     public void add(User user) throws SQLException {
         Connection c = dataSource.getConnection();
-
-        PreparedStatement ps = c.prepareStatement("insert into users (id,name,password) values(?,?,?)");
-        ps.setString(1, user.getId());
-        ps.setString(2, user.getName());
-        ps.setString(3, user.getPassword());
-        ps.executeUpdate();
-        ps.close();
-        c.close();
+        jdbcContextWithStrategy(new AddStatement(user));
     }
 
     public User get(String id) throws SQLException {
